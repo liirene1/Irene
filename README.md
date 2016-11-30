b5a4552a33bf20b81fe71fd971ab42c290956451
Irene Li <li.irene1@gmail.com>
2016-11-29 23:13:08 -0800

    added resume

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 Irene Li Personal Website
 
 TO DOs:
-1. Fix spacing on heading for my headshot + intro (once I get a good background image)
-2. "More" chevron - display + add click functionality
-3. Nav - link click functionality + change color once I get a good background image + fix styling on sticky nav
-4. Resume - opens up in new tab
-5. About - centered, spacing
-6. Projects - image links to project page, fix sizing on the images, fix sizing on each box, fix alignment on the text and button. make all 3 same size
-7. Pick a new theme color - not orange
-8. Contact Form - connect to a database? or email me.
-9. May need contrast between footer and contact form
-10. RESPONSIVENESS
-11. Test it on different browsers - use Atom auto prefixer
+1. Fix spacing on heading for my headshot + intro + chevron (once I get a good background image)
+2. Nav - fix styling on sticky nav
+3. About - centered, spacing
+4. Projects - image links to project page, fix sizing on the images, fix sizing on each box, fix alignment on the text and button. make all 3 same size
+5. Pick a new theme color for underlines (nav) and buttons- not orange
+6. Contact Form - connect to a database? or email me.
+7. May need contrast between footer and contact form
+8. RESPONSIVENESS (including navigation video)
+9. Test it on different browsers - use Atom auto prefixer
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   /* For the sticky navigation */
   $('.section-about').waypoint(function(direction) {
       if (direction == "down") {
@@ -7,6 +8,53 @@ $(document).ready(function() {
           $('nav').removeClass('sticky');
       }
   }, {
-    offset: '60px;'
+    offset: '650px;'
   });
+
+  //Scroll on button for more
+  $('.chevron').click(function () {
+    $('html, body').animate({scrollTop: $('.section-about').offset().top}, 1000);
+  });
+
+  //Navigation scroll
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+//Animations on scroll
+  $('.skills-portion').waypoint(function(direction) {
+      $('.skills-portion').addClass('animated fadeIn');
+  }, {
+      offset: '95%'
+  });
+
+  $('.passions-portion').waypoint(function(direction) {
+      $('.passions-portion').addClass('animated fadeIn');
+  }, {
+      offset: '95%'
+  });
+
+  $('.reverie').waypoint(function(direction) {
+    $('.reverie').addClass('animated fadeInRight');
+  }, {
+      offset: '85%'
+  });
+
+  $('.new-project').waypoint(function(direction) {
+      $('.new-project').addClass('animated fadeInLeft');
+  }, {
+      offset: '85%'
+  });
+
 });
